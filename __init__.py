1724abe0f4290ea285ccebfa607dd0a4d10a112b
Thedestroyerte <mohdfarhan8859@gmail.com>
2024-05-18 14:00:46 +0530

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -1,3 +1,7 @@
-# Powered By @Zero_nxz @professor
-
-from .config import *
+from .Apple import AppleAPI
+from .Carbon import CarbonAPI
+from .Resso import RessoAPI
+from .Soundcloud import SoundAPI
+from .Spotify import SpotifyAPI
+from .Telegram import TeleAPI
+from .Youtube import YouTubeAPI
