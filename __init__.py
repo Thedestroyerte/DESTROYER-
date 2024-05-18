ce24ffa0c6d46e3a59f5d65121c3bbe8b0526e25
Thedestroyerte <mohdfarhan8859@gmail.com>
2024-05-18 13:38:30 +0530

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -1,32 +1,3 @@
-from Zero.core.bot import ZeroBot
-from Zero.core.dir import dirr
-from Zero.core.git import git
-from Zero.core.userbot import Userbot
-from Zero.misc import dbb, heroku
+# Powered By @Zero_nxz @professor
 
-from .logging import LOGGER
-
-git()
-
-
-dirr()
-
-dbb()
-
-heroku()
-
-# Clients
-app = ZeroBot()
-
-userbot = Userbot()
-
-
-from .platforms import *
-
-YouTube = YouTubeAPI()
-Carbon = CarbonAPI()
-Spotify = SpotifyAPI()
-Apple = AppleAPI()
-Resso = RessoAPI()
-SoundCloud = SoundAPI()
-Telegram = TeleAPI()
+from .config import *
